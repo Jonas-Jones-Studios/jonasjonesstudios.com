888def652e9dffe8c10a3c417609823901b60345
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-11-10 03:41:06 +0100

    temporary placeholder website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import logo_banner_compact from '$lib/images/jonasjonesstudios_banner-compact.png';
+</script>
+
+<svelte:head>
+	<title>JONAS JONES STUDIOS</title>
+	<meta name="description" content="Jonas Jones Studios Homepage" />
+</svelte:head>
+
+<section>
+	<img src={logo_banner_compact} alt="Jonas Jones Studios" width="50%" />
+	<h1>>>></h1>
+	<h1>Coming soon</h1>
+</section>
+
+<div class="temp-footer">
+	<h2>by Jonas_Jones at <a href="https://jonasjones.dev">jonasjones.dev</a></h2>
+</div>
